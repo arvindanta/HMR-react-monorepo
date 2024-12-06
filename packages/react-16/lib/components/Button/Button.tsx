544a79ed6f0a611412b5268bc3544c57edaef65e
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2024-12-06 22:06:10 +0530

    changes

--- packages/react-16/lib/components/Button/Button.tsx
+++ packages/react-16/lib/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 function Button({ onClick }: { onClick: () => void }): JSX.Element {
-  return <button onClick={onClick}> aassdassd react 16 </button>;
+  return <button onClick={onClick}> aassdasasssd react 16 </button>;
 }
 
 export { Button };
